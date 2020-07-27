14db27a05181522879d2e9deb36fa2c757ea0f60
chinhcool <nineh2808@gmail.com>
2020-07-27 23:27:46 +0700

    first commit
    first commit

---
+++ tempData.js
@@ -0,0 +1,87 @@
+//Temporary Data until we connect with Firebase
+
+export default tempData = [
+    {
+        id: 1,
+        name: "Plan a Trip",
+        color: "#24A6D9",
+        todos: [
+            {
+                title: " Book Flight",
+                completed: false
+            },
+            {
+                title: " Passport Check",
+                completed: true
+            },
+            {
+                title: " Reserve Hotel Room",
+                completed: true
+            },
+            {
+                title: " EE Flight",
+                completed: false
+            }
+
+        ]
+    },
+
+    {
+        id: 2,
+        name: "Errands",
+        color: "#8022D9",
+        todos: [
+            {
+                title: " Buy Milk",
+                completed: false
+            },
+            {
+                title: " Plan weekend",
+                completed: true
+            },
+            {
+                title: " Run 2 miles",
+                completed: false
+            },
+            {
+                title: " Make video",
+                completed: true
+            },
+            {
+                title: " take dog",
+                completed: true
+            },
+
+        ]
+    },
+
+    {
+        id: 3,
+        name: "Birthday Party",
+        color: "#595BD9",
+        todos: [
+            {
+                title: " Get Balloons",
+                completed: false
+            },
+            {
+                title: " Send Invitations",
+                completed: true
+            },
+            {
+                title: " Make Dinner",
+                completed: true
+            },
+            {
+                title: " Make video",
+                completed: true
+            },
+            {
+                title: " take dog",
+                completed: true
+            },
+
+        ]
+    },
+
+]
